202364d9266e0523e3279a95482d984519782591
Pavel Samar <Spa15@mail.ru>
2025-11-10 17:36:36 +0300

    header rename

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
+import Header from "../Header_/Header";
 import Footer from "../Footer/Footer";
 
 export default function Layout() {
